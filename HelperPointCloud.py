3cb738f5507d9f24d31de25b6058279b7e7527d9
Adrian Buzatu <adrian.buzatu@cern.ch>
2019-07-31 17:39:43 +0200

    Update HelperPointCloud with functions that match two point clouds and find deviations

--- HelperPointCloud.py
+++ HelperPointCloud.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python
 
+from HelperStatistics import *
 from plyfile import PlyData, PlyElement
 import numpy as np
+from sklearn.neighbors import NearestNeighbors
 
-def get_plyData_from_ply_ascii_file(inputFileName,debug):
+def get_plyData_from_ply_ascii_file(inputFileName,debug=False):
     if debug:
         print "Reading plyData from ply ascii file",inputFileName
     plyData=PlyData.read(inputFileName)
@@ -52,6 +54,12 @@ def get_list_point(plyData,expected_M=0,debug=False,verbose=False):
     return list_point
 # done function
 
+def get_list_point_from_ply_ascii_file(inputFileName,expected_M=0,debug=False,verbose=False):
+    if debug:
+        print "Reading numpy from ply ascii file",inputFileName
+    return get_list_point(PlyData.read(inputFileName),expected_M=0,debug=debug,verbose=debug)
+# done function
+
 # get a point cloud of N points, each with M properties (x,y,z, etc)
 # as a numpy array of shape N*M
 # each row is a new point of dimension M; so there are M columns
@@ -60,7 +68,7 @@ def get_nparray(plyData,expected_M=0,debug=False,verbose=False):
     list_pointTuple=plyData.elements[0].data
     N=len(list_pointTuple)
     M=len(list_pointTuple[0])
-    if debug or verbose:
+    if debug:
         print "N",N,"M",M
     if debug:
         print "plyData",type(plyData),plyData
@@ -82,17 +90,24 @@ def get_nparray(plyData,expected_M=0,debug=False,verbose=False):
         print "nparray after filling"
         print nparray  
     # calculate the min and max for each dimension
-    if debug or verbose:
+    if debug:
         print "Calculate min and max on each axis:"
     for i in xrange(M):
         nparrayD=nparray[:,i]
         minValue=np.min(nparrayD)
         maxValue=np.max(nparrayD)
-        if debug or verbose:
+        if debug:
             print "axis %i: (min,max)=(%.3f, %.3f);" % (i,minValue,maxValue)
-    print ""
-    # below example how to sort the numpy array 
+    if debug:
+        print ""
+    # done
     return nparray
+# done function
+
+def get_nparray_from_ply_ascii_file(inputFileName,expected_M=0,debug=False,verbose=False):
+    if debug:
+        print "Reading numpy from ply ascii file",inputFileName
+    return get_nparray(PlyData.read(inputFileName),expected_M=0,debug=debug,verbose=verbose)
 # done function
 
 # write a point cloud represented as a nparray to an ASCII .ply file
@@ -105,6 +120,10 @@ def write_point_cloud_to_ascii_ply(nparray, outputFileName,debug=False,verbose=F
         print nparray
     N=nparray.shape[0]
     M=nparray.shape[1]
+    if debug:
+        print "N",N,"M",M
+    if M!=3:
+        print "WARNING!!! The header for M",M,"may not be right. Check the header writes all the variables you want!!!"
     if debug:
         for i in xrange(N):
             print i,nparray[i]
@@ -163,12 +182,115 @@ def test_one_point_cloud(inputFileName,debug=False,verbose=False):
     plyData=get_plyData_from_ply_ascii_file(inputFileName,debug)
     if debug or verbose:
         print "list_point:"
-    list_point=get_list_point(plyData,expected_M=0,debug=False,verbose=verbose)
+    list_point=get_list_point(plyData,expected_M=0,debug=debug,verbose=verbose)
     N=len(list_point)
     if debug or verbose:
         print "N",N,"[0]",type(list_point[0]),list_point[0]
         print "numpy NxM:"
-    nparray=get_nparray(plyData,expected_M=0,debug=False,verbose=verbose)
+    nparray=get_nparray(plyData,expected_M=0,debug=debug,verbose=verbose)
     testFileName=inputFileName.replace(".ply","_rewrite.ply")
-    write_point_cloud_to_ascii_ply(nparray,testFileName,debug=False,verbose=verbose)
+    write_point_cloud_to_ascii_ply(nparray,testFileName,debug=debug,verbose=verbose)
+    # to add: function that re-orders the axis to be in a particular order to be plotted easily:
+    # left to right; bottom to up; backwards to forwards; the attributes remain as they are
+    # create histograms for all the axes
+# done function
+
+def distance(nparray1,nparray2,debug=False):
+    M1=len(nparray1)
+    M2=len(nparray2)
+    if debug:
+        print "M1",M1,"M2",M2
+    assert(M1==M2)
+    M=M2
+    distance_squared=0
+    for i in xrange(M):
+        distance_squared+=(nparray1[i]-nparray2[i])**2
+    distance=math.sqrt(distance_squared)
+    if debug:
+        print "distance",distance
+    return distance
+# done function
+
+def reorder_one_point_cloud_to_match_a_reference(pc_reference,pc_current,debug=False,verbose=False):
+    # for each get a subset representing only the coordinates
+    # update_dictionary_plyData_retrieve_dictionary_distance
+    # get_dict_stage_name_values(dict_stage_plyData,dataFormat)
+    #
+    # use all coordinates in the point cloud (this is just an approximation)
+    if debug:
+        print "Built NearestNeigbourghs object for the pc_current"
+    nn_current=NearestNeighbors(n_neighbors=1,algorithm='auto',metric='euclidean').fit(pc_current)
+    if debug:
+        print "Match the pc_reference to the pc_current"
+    distances_current_to_reference,indices_current_to_reference=nn_current.kneighbors(pc_reference)
+    if debug:
+        print "distances_current_to_reference",type(distances_current_to_reference),len(distances_current_to_reference),distances_current_to_reference
+        print "indices_current_to_reference",type(indices_current_to_reference),len(indices_current_to_reference),indices_current_to_reference
+    if False:
+        for i in xrange(len(distances_current_to_reference)):
+            print "i",i,"reference",pc_reference[i],"current_ordered",pc_current[indices_current_to_reference[i]][0],"current_original",pc_current[i]
+            print pc_reference[i].shape
+            print pc_current[indices_current_to_reference[i]][0].shape
+    if debug:
+        print "indices_current_to_reference",indices_current_to_reference.shape
+        print "Reshape the distances and indices to be from (N,1) to (N,)"
+    indices_current_to_reference_reshaped=indices_current_to_reference.T[0]
+    distances_current_to_reference_reshaped=distances_current_to_reference.T[0]
+    if debug:
+        print "indices_current_to_reference_reshaped",indices_current_to_reference_reshaped
+    if debug:
+        print "Reorder the pc_current to match the order in pc_reference"
+    pc_current_reordered=pc_current[indices_current_to_reference_reshaped]
+    if debug:
+        print "pc_current",pc_current.shape,pc_current[0]
+        print pc_current
+        print "pc_reference",pc_reference.shape,pc_reference[0]
+        print pc_reference
+        print "pc_current_reordered",pc_current_reordered.shape,pc_current_reordered[0]
+        print pc_current_reordered
+        print "distances_current_to_reference_reshaped"
+        print distances_current_to_reference_reshaped
+    print ""
+    N=pc_current.shape[0]
+    if debug:
+        for i in xrange(N):
+            if i>10:
+                continue
+            dist_us=distance(pc_reference[i],pc_current_reordered[i],debug)
+            dist_them_1=distances_current_to_reference_reshaped[i]
+            print i,"cur",pc_current[i],"ref",pc_reference[i],"cur_ordered",pc_current_reordered[i],"dist_us",dist_us,"dist_them",dist_them_1
+    # done
+    return pc_current_reordered,distances_current_to_reference_reshaped
+# done function
+
+def compare_two_point_clouds_with_same_order(pc_current,pc_reference,debug=False,verbose=False):
+    pc_current_deviation=np.absolute(pc_current-pc_reference)
+    if debug:
+        print "pc_current_deviation"
+        print pc_current_deviation
+    N,M=pc_current.shape
+    if debug:
+        print "N",N,"M"
+    # get numpy arrays of dimension M with the maximum and averages values of each column
+    columns_max=np.max(pc_current_deviation,0)
+    columns_avg=np.mean(pc_current_deviation,0)
+    # and here have access at the values of each column
+    # such as to store in a histogram and then plot them
+    if False:
+        for i in xrange(M):
+            if debug:
+                print "axis i",i
+            # the column along the i-th axis
+            # 1 is the second dimension, hence the one with axis
+            # then from the axes we take the one with number i
+            nparray_column=np.take(pc_current_deviation,i,1)
+            column_max=np.max(nparray_column)
+            column_avg=np.mean(nparray_column)
+            if debug:
+                print "max",column_max,"avg",column_avg
+            # create a histogram from the full distribution of the column
+            # and save it in a plot
+        # done loop over dimensions
+    # done if
+    return columns_max,columns_avg
 # done function
