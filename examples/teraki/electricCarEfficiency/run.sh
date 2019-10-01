3e392735d64d47b418e35e05c865c7ff1b64167c
abuzatu <adrian.buzatu@cern.ch>
2019-10-01 14:56:54 +0200

    remove electricCarEfficiency

--- examples/teraki/electricCarEfficiency/run.sh
+++
@@ -1,5 +0,0 @@
-# produce plots
-rm -f output/*.png && ./electricCarEfficiency.py && ls -lh output/ && open ./output/*.png
-# ./electricCarEfficiency.py && ls -lh output/ 
-# produce slides
-cd slides && pdflatex electricCarEfficiency.tex && open electricCarEfficiency.pdf && cd ..
