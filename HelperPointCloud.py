eed0ee891992da2eda8b499a45971cfb218534de
abuzatu <adrian.buzatu@cern.ch>
2020-02-01 15:48:20 +0100

    HelperPointCloud.py add function to compute euclidean distance from spherical coordinates

--- HelperPointCloud.py
+++ HelperPointCloud.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 
 import numpy as np
+import math
+from HelperNumpy import print_numpy
+from HelperMatplotlib import *
 
 def get_nparray_from_structured_array_from_binary_file(inputFileName,expected_M=0,debug=False,verbose=False):
     if debug:
@@ -91,6 +94,12 @@ def reorder_one_point_cloud_to_match_a_reference_with_last_element_an_id_new_way
 # done function
 
 def compare_two_point_clouds_with_same_order(pc_current,pc_reference,debug=False,verbose=False):
+    if True:
+        print("pc_current")
+        print(pc_current)
+        print("pc_reference")
+        print(pc_reference)
+    # calculate the max and avg deviation for each axis
     pc_current_deviation=np.absolute(pc_current-pc_reference)
     if debug:
         print("pc_current_deviation")
@@ -101,10 +110,14 @@ def compare_two_point_clouds_with_same_order(pc_current,pc_reference,debug=False
     # get numpy arrays of dimension M with the maximum and averages values of each column
     columns_max=np.max(pc_current_deviation,0)
     columns_avg=np.mean(pc_current_deviation,0)
+    if True or debug:
+        print("columns_max",columns_max)
+        print("columns_avg",columns_avg)
+    # assert(False)
     # and here have access at the values of each column
     # such as to store in a histogram and then plot them
     if False:
-        for i in xrange(M):
+        for i in range(M):
             if debug:
                 print("axis i",i)
             # the column along the i-th axis
@@ -113,11 +126,187 @@ def compare_two_point_clouds_with_same_order(pc_current,pc_reference,debug=False
             nparray_column=np.take(pc_current_deviation,i,1)
             column_max=np.max(nparray_column)
             column_avg=np.mean(nparray_column)
-            if debug:
+            if True or debug:
                 print("max",column_max,"avg",column_avg)
             # create a histogram from the full distribution of the column
             # and save it in a plot
         # done loop over dimensions
     # done if
     return pc_current_deviation,columns_max,columns_avg
+# done function
+
+def calculate_euclidean_distance_deviation_from_point_clouds_with_same_order(pc_current,pc_reference,S,coordinateSystemType,debug=False,verbose=False):
+    N,M=pc_current.shape
+    pc_current=pc_current.astype(np.float64)
+    pc_reference=pc_reference.astype(np.float64)
+    if True or debug:
+        print("N",N,"M",M,"S",S)
+        print("pc_current","dtype",pc_current.dtype)
+        print(pc_current)
+        print("pc_reference","dtype",pc_reference.dtype)
+        print(pc_reference)
+        columns_max=np.max(pc_current,0)
+        print("columns_max",columns_max)
+        columns_max=np.max(pc_reference,0)
+        print("columns_max",columns_max)
+    #assert(False)
+    # loop over all points and for each calculate the euclidean distance
+    # support both cartesian and spherical, and S=0, 1, or 2. 
+    list_d_cartesian=[]
+    list_d_spherical=[]
+    list_r2=[]
+    list_t2=[]
+    list_p2=[]
+    list_r1=[]
+    list_t1=[]
+    list_p1=[]
+    for i in range(N):
+        #if i>10:
+        #    continue
+        point_current=pc_current[i]
+        point_reference=pc_reference[i]
+        if debug:
+            print("i",i,"point_current",point_current,"dtype",point_current.dtype,"point_reference",point_reference,"dtype",point_reference.dtype)
+        if coordinateSystemType=="Cartesian":
+            if S==1:
+                pass
+            elif S==2:
+                pass
+            elif S==3:
+                pass
+            else:
+                print("S",S,"not known. Choose 1, 2, 3. Will ABORT!!!")
+            # done if
+        elif coordinateSystemType=="Spherical":
+            if S==1:
+                pass
+            elif S==2:
+                pass
+            elif S==3:
+                # read variables from point_current, assume order radius, theta, phi
+                r2=point_current[0]
+                t2=point_current[1]
+                p2=point_current[2]
+                #t2=0
+                #p2=0
+                # read variables from point_reference, assume order radius, theta, phi
+                r1=point_reference[0]
+                t1=point_reference[1]
+                p1=point_reference[2]
+                #t1=0
+                #p1=0
+                if r2==0 or r1==0:
+                    continue
+                # calculate euclidean distance
+                cos_t2_t1=np.cos(t2-t1)
+                cos_p2_p1=np.cos(p2-p1)
+                sin_t2=np.sin(t2)
+                sin_t1=np.sin(t1)
+                cos_t2=np.cos(t2)
+                cos_t1=np.cos(t1)
+                if debug:
+                    print("r2",r2,"type",r2.dtype)
+                    print("r1",r1,"type",r1.dtype)
+                    print("t2",t2)
+                    print("t1",t1)
+                    print("t2_t1",t2-t1)
+                    print("cos_t2",cos_t2)
+                    print("cos_t1",cos_t1)
+                    print("cos_t2_t1",cos_t2_t1)
+                    print("sin_t2",sin_t2,"type",type(sin_t2),math.sin(t2),type(math.sin(t2)))
+                    print("sin_t1",sin_t1)
+                    print("p2",p2,"p1",p1)
+                    print("r2*r2",r2*r2)
+                    print("r1*r1",r1*r1)
+                # d*d=r2*r2+r1*r2-2*r2*r1*[cos(t2-t1)*cos(p2-p1)+sin(t1)*sin(t2)*[1-cos(p2-p1]]
+                Parantheses_max_allowed=(r2*r2+r1*r1)/(2*r2*r1)
+                if debug:
+                    print("Parantheses_max_allowed",Parantheses_max_allowed)
+                Parantheses_1=(cos_t2_t1*cos_p2_p1+sin_t2*sin_t1*(1.0-cos_p2_p1))
+                if debug:
+                    print("Parantheses_1",Parantheses_1)
+                d2_spherical=r2*r2+r1*r1-2*r2*r1*Parantheses_1
+                d_spherical=np.sqrt(d2_spherical)
+                if debug:
+                    print("d2_spherical",d2_spherical,"d_spherical",d_spherical)
+                # calculate also cartesian coordinates and then the euclidean distance to validate the spherical coordinates formula
+                #
+                x2=r2*cos_t2*cos_t2
+                y2=r2*cos_t2*sin_t2
+                z2=r2*sin_t2
+                #
+                x1=r1*cos_t1*cos_t1
+                y1=r1*cos_t1*sin_t1
+                z1=r1*sin_t1
+                #
+                x2_x1=x2-x1
+                y2_y1=y2-y1
+                z2_z1=z2-z1
+                #
+                d2_cartesian=x2_x1*x2_x1+y2_y1*y2_y1+z2_z1*z2_z1
+                d_cartesian=np.sqrt(d2_cartesian)
+                if debug:
+                    print("d2_cartesian",d2_cartesian,"d_cartesian",d_cartesian)
+         
+                #Peak=2
+                #if (d_cartesian>Peak-0.1 and d_cartesian<Peak+0.1)==False:
+                #    continue
+                #
+                list_d_cartesian.append(d_cartesian)
+                list_d_spherical.append(d_spherical)
+                list_r1.append(r1)
+                list_t1.append(t1)
+                list_p1.append(p1)
+                list_r2.append(r2)
+                list_t2.append(t2)
+                list_p2.append(p2)
+            else:
+                print("S",S,"not known. Choose 1, 2, 3. Will ABORT!!!")
+            # done if
+        else:
+            print("coordinateSystemType",coordinateSystemType,"not known. Choose Cartesian or Spherical. Will ABORT!!!")
+            assert(False)
+        # done if
+    # done loop over all points
+    #
+    numpy_d_cartesian=np.array(list_d_cartesian)
+    numpy_d_spherical=np.array(list_d_spherical)
+    numpy_r1=np.array(list_r1)*0.01
+    numpy_t1=np.array(list_t1)#*180/np.pi
+    numpy_p1=np.array(list_p1)#*180/np.pi
+    numpy_r2=np.array(list_r2)*0.01
+    numpy_t2=np.array(list_t2)#*180/np.pi
+    numpy_p2=np.array(list_p2)#*180/np.pi
+    if True or debug:
+        print_numpy("numpy_d_cartesian",numpy_d_cartesian)
+        print_numpy("numpy_d_spherical",numpy_d_spherical)
+        print_numpy("numpy_r1",numpy_r1)
+        print_numpy("numpy_t1",numpy_t1)
+        print_numpy("numpy_p1",numpy_p1)
+        print_numpy("numpy_r2",numpy_r2)
+        print_numpy("numpy_t2",numpy_t2)
+        print_numpy("numpy_p2",numpy_p2)
+        # make plots
+        #binning_deviation=Peak-0.05+np.array(range(0,20))*(0.1/20.0)
+        binning_deviation=0.0+15.0*np.array(range(0,100))/100.0
+        draw_histogram_2d(numpy_d_cartesian,numpy_d_spherical,outputFileName="./output_histo_2D",extensions="png,pdf",nrBins=binning_deviation,info_x=["cartesian"],info_y=["spherical"],title="Title",plotColorBar=True,debug=False,verbose=False)
+        draw_histogram_2d(numpy_d_cartesian,numpy_r1,outputFileName="./output_histo_2D_cartesian_radius",extensions="png,pdf",nrBins=[binning_deviation,100],info_x=["cartesian"],info_y=["radius"],title="Title",plotColorBar=True,debug=False,verbose=False)
+        draw_histogram_2d(numpy_d_cartesian,numpy_t1,outputFileName="./output_histo_2D_cartesian_theta",extensions="png,pdf",nrBins=[binning_deviation,100],info_x=["cartesian"],info_y=["theta"],title="Title",plotColorBar=True,debug=False,verbose=False)
+        draw_histogram_2d(numpy_d_cartesian,numpy_p1,outputFileName="./output_histo_2D_cartesian_phi",extensions="png,pdf",nrBins=[binning_deviation,100],info_x=["cartesian"],info_y=["phi"],title="Title",plotColorBar=True,debug=False,verbose=False)
+
+        draw_histogram_2d(numpy_d_cartesian,np.cos(numpy_t1-numpy_t2),outputFileName="./output_histo_2D_cartesian_cos_theta",extensions="png,pdf",nrBins=[binning_deviation,100],info_x=["cartesian"],info_y=["cos theta"],title="Title",plotColorBar=True,debug=False,verbose=False)
+        draw_histogram_2d(numpy_d_cartesian,np.cos(numpy_p1-numpy_p2),outputFileName="./output_histo_2D_cartesian_cos_phi",extensions="png,pdf",nrBins=[binning_deviation,100],info_x=["cartesian"],info_y=["cos phi"],title="Title",plotColorBar=True,debug=False,verbose=False)
+
+
+        draw_histogram_2d(numpy_r1,numpy_r2,outputFileName="./output_histo_2D_r",extensions="png,pdf",nrBins=100,info_x=["r1"],info_y=["r2"],title="Title",plotColorBar=True,debug=False,verbose=False)
+        draw_histogram_2d(numpy_t1,numpy_t2,outputFileName="./output_histo_2D_t",extensions="png,pdf",nrBins=100,info_x=["t1"],info_y=["t2"],title="Title",plotColorBar=True,debug=False,verbose=False)
+        draw_histogram_2d(numpy_p1,numpy_p2,outputFileName="./output_histo_2D_p",extensions="png,pdf",nrBins=100,info_x=["p1"],info_y=["p2"],title="Title",plotColorBar=True,debug=False,verbose=False)
+        #
+        list_tupleArray=[]
+        list_tupleArray.append((numpy_d_cartesian,"Carthesian"))
+        list_tupleArray.append((numpy_d_spherical,"Spherical"))
+        overlay_histogram_from_nparray_with_ratio(list_tupleArray,outputFileName="./output_histo_from_nparray",extensions="png,pdf",nrBins=binning_deviation,histtype="step",info_x=["deviation"],info_y=["Number of points"],title="Title",text=None,info_legend=["best"],list_color="r,g,b,k,y".split(","),doAddRatioPad=False,debug=False,verbose=False)
+    # done if
+    # ready to return
+    return numpy_d_spherical
 # done function
